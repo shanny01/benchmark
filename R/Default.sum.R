1faaaad47fe20bda31dab2f8ec7efa767a58b67a
shanny01 <shanny01@foxmail.com>
2022-04-09 03:42:20 +0800

    Update Default.sum.R

--- R/Default.sum.R
+++ R/Default.sum.R
@@ -1,6 +1,6 @@
 #' Cell–cell interactions in human metastatic melanoma predicted by CellChat, CellPhoneDB, Connectome, iTALK, NATMI,  and SingleCellSignalR using the default LR resources based on the sum of ligand-receptor scores
-#' @docType data
-#'
+#'@docType data
+#'@name Default.sum
 #'
 #'
 #'
