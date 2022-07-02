eeddbd91807326da9ce20cdfe2ed81fb41531a2c
shanny01 <shanny01@foxmail.com>
2022-07-02 15:32:19 +0800

    RData

---
+++ R/Bray.Curtis.score_Enrichment.score.R
@@ -0,0 +1,9 @@
+#' Cell–cell interactions in human metastatic melanoma based on Bray-Curtis score and the enrichment score using the concensus LR resource in LIANA.
+#' @docType data
+#' @name Bray.Curtis.score_Enrichment.score
+#' @source data('Bray.Curtis.score_Enrichment.score')
+#'
+#'
+#'
+#'
+"Bray.Curtis.score_Enrichment.score"
