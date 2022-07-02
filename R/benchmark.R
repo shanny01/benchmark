d0e858406bfd9b07019012119b135b65d4221bf7
shanny01 <shanny01@foxmail.com>
2022-07-03 00:37:43 +0800

    Update benchmark.R

--- R/benchmark.R
+++ R/benchmark.R
@@ -26,7 +26,6 @@
 #' input = do.call(rbind,Bray.Curtis.score_Enrichment.score)
 #' benchmark(assess = input,directed = F)
 benchmark <- function(assess,directed){
-  data('CITEdb.RData')
   CITEdb.1 = CITEdb[CITEdb$Information==1,]
 
   if(directed == T){
