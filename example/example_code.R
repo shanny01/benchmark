4e71b25f3a860fa55d72287a1d729ad69dbfe722
shanny01 <shanny01@foxmail.com>
2022-07-03 00:14:02 +0800

    Update example_code.R

--- example/example_code.R
+++ example/example_code.R
@@ -3,13 +3,22 @@ library(dplyr)
 library(yardstick)
 library(benchmark)
 
-## load example data
-## Cell–cell interactions in human metastatic melanoma predicted by CellChat, CellPhoneDB, Connectome, iTALK, NATMI,  and SingleCellSignalR using the default LR resources based on the sum of ligand-receptor scores
+## load example data with directed cell-cell interactions
+## Cell–cell interactions in human metastatic melanoma predicted by CellChat, CellPhoneDB, Connectome, iTALK, NATMI,  and SingleCellSignalR using the default LR resources based on the sum of ligand-receptor scores.
 data('Default.sum')
 
 ## benchmark one algorithm
-benchmark(assess = Default.sum[[1]])
+benchmark(assess = Default.sum[[1]],directed = T)
 
-##benchmark multiple algorithms at once
+## benchmark multiple algorithms at once
 input = do.call(rbind,Default.sum)
-benchmark(assess = input)
+benchmark(assess = input,directed = T)
+
+## load example data with undirected cell-cell interactions
+## #' Cell–cell interactions in human metastatic melanoma based on Bray-Curtis score and the enrichment score using the concensus LR resource in LIANA.
+data('Bray.Curtis.score_Enrichment.score')
+
+## benchmark multiple algorithms at once
+input = do.call(rbind,Bray.Curtis.score_Enrichment.score)
+benchmark(assess = input,directed = F)
+
