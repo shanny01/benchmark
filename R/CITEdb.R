c7b2d99fc973a3b5227516198187c791093aca7e
shanny01 <shanny01@foxmail.com>
2022-07-02 16:01:16 +0800

    Creat CITEdb.R

---
+++ R/CITEdb.R
@@ -0,0 +1,8 @@
+#' CITEdb(https://citedb.cn/#/), a cell-cell interaction database at cellular level that has been manually curated from literatures, provides an initial benchmark dataset when interpreting and evaluating the computational results of cell-cell interactions derived from different tools.
+#' @name CITEdb
+#' @source data('CITEdb')
+#'
+#'
+#'
+#'
+"CITEdb"
